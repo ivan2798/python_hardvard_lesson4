9e4e8a49c2b617f3b17c2959300ee7c76b7bcc40
ivan2798 <inge1998@hotmail.com>
2022-09-30 11:41:26 -0500

    Function Arguments

--- number.py
+++ number.py
@@ -1,9 +1,13 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an Integer")
-    else:
-        break 
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
 
-print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+main()
